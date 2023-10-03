04706c5c0f5834aced6535895d79f59d47f9bafc
Seth Roberts <sethknock@gmail.com>
2023-10-03 02:57:08 +0000

    Modified OpenAI key to use Github secret storage

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
-from apikey import apikey
 
 import streamlit as st
 from langchain.llms import OpenAI
 
-os.environ['OPENAI_API_KEY'] = apikey
+os.environ['OPENAI_API_KEY'] = process.env.{OPENAI_API_KEY}
+
 
 #App framework
 st.title('🦜🔗 YouTube GPT Creator')
