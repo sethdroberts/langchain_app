4e96c23945537508fc064a57b15d64e1cdbbf059
Ubuntu <ubuntu@ip-172-31-33-198.us-east-2.compute.internal>
2023-10-03 02:32:45 +0000

    Initiate repo

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from apikey import apikey
+
+import streamlit as st
+from langchain.llms import OpenAI
+
+os.environ['OPENAI_API_KEY'] = apikey
+
+#App framework
+st.title('🦜🔗 YouTube GPT Creator')
+prompt = st.text_input('Plug in your prompt here')
